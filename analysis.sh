df594fc06ac40ffdcaf59a0eff503ef0c49faba4
Aryan <aaryanraj.115@gmail.com>
2026-08-28 18:07:13 +0530

    ME branch info

--- analysis.sh
+++ analysis.sh
@@ -10,5 +10,6 @@ fi
 echo "Total number of lines: $(wc -l < "$file")"
 echo "Number of CSE students: $(grep -c "CSE" "$file")"
 echo "Number of EE students: $(grep -c "EE" "$file")"
+echo "Number of ME students: $(grep -c "ME" "$file")"
 
 echo "Analysis Completed"
