4aab13b5c9b5b74b6986128c251d4b23273f1cd1
Aryan <aaryanraj.115@gmail.com>
2026-08-28 18:06:36 +0530

    added hostel info

--- analysis.sh
+++ analysis.sh
@@ -10,5 +10,6 @@ fi
 echo "Total number of lines: $(wc -l < "$file")"
 echo "Number of CSE students: $(grep -c "CSE" "$file")"
 echo "Number of EE students: $(grep -c "EE" "$file")"
+echo "Number of students in Hostel 4: $(grep -c "Hostel 4" "$file")"
 
 echo "Analysis Completed"
