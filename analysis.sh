c9d63c7f9acd4ede169d9147759183c931c3dacf
Aryan <aaryanraj.115@gmail.com>
2026-08-28 17:57:46 +0530

    initial commit

---
+++ analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file="students.csv"
+
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' does not exist."
+    exit 1
+fi
+
+echo "Total number of lines: $(wc -l < "$file")"
+echo "Number of CSE students: $(grep -c "CSE" "$file")"
+echo "Number of EE students: $(grep -c "EE" "$file")"
+
+echo "Analysis Completed"
